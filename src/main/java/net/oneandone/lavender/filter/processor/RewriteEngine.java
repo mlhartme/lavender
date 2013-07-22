ff481cfe3863599e75de6040f7f107ce252a2bd6
Michael Hartmeier <mlhartme@github.com>
2013-07-22 15:09:41 +0200

    merge UrlCalculator into RewriteEngine

--- src/main/java/net/oneandone/lavender/filter/processor/RewriteEngine.java
+++ src/main/java/net/oneandone/lavender/filter/processor/RewriteEngine.java
@@ -20,9 +20,16 @@ import net.oneandone.lavender.index.Label;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * RewriteEngine that is able to load and push resources during rewrite.
@@ -31,11 +38,55 @@ public class RewriteEngine {
     private static final Logger LOG = LoggerFactory.getLogger(RewriteEngine.class);
 
     protected final Index index;
-    protected final UrlCalculator urlCalculator;
 
-    public RewriteEngine(Index index, UrlCalculator urlCalculator) {
+    /** The nodes used for HTTP */
+    protected final Map<String, URI> httpNodes = new HashMap<>();
+
+    /** The nodes used for HTTPS */
+    protected final Map<String, URI> httpsNodes = new HashMap<>();
+
+    /** The consistent hash function. */
+    protected final ConsistentHash consistentHash;
+
+    public RewriteEngine(Index index, URL nodesUrl) throws IOException {
         this.index = index;
-        this.urlCalculator = urlCalculator;
+        InputStream in;
+
+        in = nodesUrl.openStream();
+        if (in == null) {
+            throw new IllegalStateException("nodes file not found: " + nodesUrl);
+        }
+        readNodes(in);
+        in.close();
+        this.consistentHash = new ConsistentHash(200, httpNodes.keySet().toArray(new String[0]));
+    }
+
+
+    private void readNodes(InputStream raw) throws IOException {
+        BufferedReader in;
+        String line;
+
+        in = new BufferedReader(new InputStreamReader(raw, Index.ENCODING));
+        while (true) {
+            line = in.readLine();
+            if (line == null) {
+                return;
+            }
+            line = line.trim();
+            if (!line.isEmpty()) {
+                if (!line.endsWith("/")) {
+                    line = line + "/";
+                }
+                URI uri = URI.create(line);
+                if ("http".equals(uri.getScheme())) {
+                    httpNodes.put(uri.getHost(), uri);
+                } else if ("https".equals(uri.getScheme())) {
+                    httpsNodes.put(uri.getHost(), uri);
+                } else {
+                    throw new IllegalArgumentException("Node " + line + " has unsupported schema, only http and https are supported.");
+                }
+            }
+        }
     }
 
     public String rewrite(String uri, URI baseURI, String contextPath) {
@@ -69,7 +120,24 @@ public class RewriteEngine {
             return reference;
         }
 
-        return urlCalculator.calculateURL(label, baseURI);
+        return calculateURL(label, baseURI);
+    }
+
+    public URI calculateURL(Label label, URI baseURI) {
+        if (label.getLavendelizedPath() == null) {
+            throw new IllegalStateException();
+        }
+        byte[] md5 = label.md5();
+        String node = consistentHash.getNodeForHash(md5);
+        String lavendelizedPath = label.getLavendelizedPath();
+        URI nodeURI = baseURI.getScheme().equals("https") ? httpsNodes.get(node) : httpNodes.get(node);
+        String path = nodeURI.getPath() + lavendelizedPath;
+        int port = nodeURI.getPort();
+        try {
+            return new URI(nodeURI.getScheme(), null, node, port, path, null, null);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     Label lookup(URI reference, URI baseURI, String contextPath) {
--- src/test/java/net/oneandone/lavender/filter/processor/RewriteEngineTest.java
+++ src/test/java/net/oneandone/lavender/filter/processor/RewriteEngineTest.java
@@ -15,13 +15,18 @@
  */
 package net.oneandone.lavender.filter.processor;
 
-import net.oneandone.lavender.filter.processor.RewriteEngine;
-import net.oneandone.lavender.filter.processor.UrlCalculator;
 import net.oneandone.lavender.index.Index;
 import net.oneandone.lavender.index.Label;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -30,6 +35,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -37,13 +43,11 @@ import static org.mockito.Mockito.when;
 
 public class RewriteEngineTest {
     private Index index;
-    private UrlCalculator urlCalculator;
     private RewriteEngine engine;
 
     @Before
     public void setUp() throws IOException {
         index = mock(Index.class);
-        urlCalculator = mock(UrlCalculator.class);
         engine = new RewriteEngine(index, urlCalculator);
     }
 
@@ -152,5 +156,59 @@ public class RewriteEngineTest {
     @Test
     public void testInvalid() throws Exception {
         assertEquals("'", engine.rewrite("'", URI.create("http://localhost:80/app/img/"), "/app/"));
+    }
+
+    //--
+
+
+    private File nodesFile;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Before
+    public void setup() throws IOException, DecoderException {
+        nodesFile = new File(tempFolder.getRoot(), "lavender.nodes");
+        String data = "";
+        data += "http://s1.uicdn.net/m1" + IOUtils.LINE_SEPARATOR;
+        data += "https://s1.uicdn.net/m1" + IOUtils.LINE_SEPARATOR;
+        data += "http://s2.uicdn.net/m1/" + IOUtils.LINE_SEPARATOR;
+        data += "https://s2.uicdn.net/m1/" + IOUtils.LINE_SEPARATOR;
+        data += IOUtils.LINE_SEPARATOR;
+        FileUtils.writeStringToFile(nodesFile, data);
+    }
+
+    @Test
+    public void testConstructor() throws IOException {
+        RewriteEngine engine = new RewriteEngine(new Index(), nodesFile.toURI().toURL());
+        assertNotNull(engine.httpNodes);
+        assertEquals(2, engine.httpNodes.size());
+        assertTrue(engine.httpNodes.keySet().contains("s1.uicdn.net"));
+        assertTrue(engine.httpNodes.keySet().contains("s2.uicdn.net"));
+        assertNotNull(engine.httpsNodes);
+        assertEquals(2, engine.httpsNodes.size());
+        assertTrue(engine.httpsNodes.keySet().contains("s1.uicdn.net"));
+        assertTrue(engine.httpsNodes.keySet().contains("s2.uicdn.net"));
+    }
+
+    @Test
+    public void testCalculateURL() throws IOException {
+        RewriteEngine engine;
+
+        engine = new RewriteEngine(new Index(), nodesFile.toURI().toURL());
+        for (char c = 'A'; c <= 'Z'; c++) {
+            byte[] md5 = DigestUtils.md5("" + c);
+            String md5hex = DigestUtils.md5Hex("" + c);
+
+            Label label = mock(Label.class);
+            when(label.getLavendelizedPath()).thenReturn(md5hex + ".png");
+            when(label.getOriginalPath()).thenReturn("logo.png");
+            when(label.md5()).thenReturn(md5);
+
+            URI baseURI = URI.create("http://somehost.somedomain.net/abc/def/xyz.html?a=b");
+            URI uri = engine.calculateURL(label, baseURI);
+            String pattern = "http://s[1-2]\\.uicdn\\.net/m1/" + md5hex + "\\.png";
+            assertTrue(uri.toString().matches(pattern));
+        }
     }
 }
