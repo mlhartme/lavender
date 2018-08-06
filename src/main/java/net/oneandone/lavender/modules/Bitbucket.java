58912d61f937d5c6061537aae1cdae7b81658d33
Michael Hartmeier <mlhartme@github.com>
2018-08-06 13:59:47 +0200

    bitbucket work

--- src/main/java/net/oneandone/lavender/modules/Bitbucket.java
+++ src/main/java/net/oneandone/lavender/modules/Bitbucket.java
@@ -15,6 +15,8 @@
  */
 package net.oneandone.lavender.modules;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.oneandone.sushi.fs.World;
@@ -23,15 +25,31 @@ import net.oneandone.sushi.fs.http.HttpNode;
 import java.io.IOException;
 
 /** As of 8-2018, we have bitbucket server 5.9.1.
- *  Rest API documentation: https://developer.atlassian.com/server/bitbucket/reference/rest-api/ */
+ *  Rest API documentation: https://docs.atlassian.com/bitbucket-server/rest/5.12.0/bitbucket-rest.html?utm_source=%2Fstatic%2Frest%2Fbitbucket-server%2Flatest%2Fbitbucket-rest.html&utm_medium=301  */
 public class Bitbucket {
     public static void main(String[] args) throws IOException {
         World world;
         Bitbucket bitbucket;
+        JsonObject obj;
+        JsonArray values;
+        JsonObject path;
+        String full;
 
         world = World.create(false);
         bitbucket = new Bitbucket((HttpNode) world.validNode("http://bitbucket.1and1.org:7990/rest/api/1.0"));
-        System.out.println(bitbucket.changes("CISOOPS", "puc", "ea01f95dafd", "cd309b8a877"));
+        System.out.println("branches" + bitbucket.branches("CISOOPS", "puc"));
+        obj = bitbucket.changes("CISOOPS", "puc", "ea01f95dafd", "cd309b8a877");
+        values = obj.get("values").getAsJsonArray();
+        for (JsonElement element : values) {
+            path = element.getAsJsonObject().get("path").getAsJsonObject();
+            full = path.get("parent").getAsString();
+            if (!full.isEmpty()) {
+                full = full + "/";
+            };
+            full = full + path.get("name").getAsString();
+            System.out.println(full);
+
+        }
     }
 
     private final HttpNode root;
@@ -40,6 +58,10 @@ public class Bitbucket {
     public Bitbucket(HttpNode root) {
         this.root = root;
         this.parser = new JsonParser();
+    }
+
+    public String branches(String project, String repository) throws IOException {
+        return root.join("projects", project, "repos", repository, "branches").readString();
     }
 
     public JsonObject changes(String project, String repository, String from, String to) throws IOException {
