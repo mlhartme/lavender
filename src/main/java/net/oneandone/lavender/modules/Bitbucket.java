9db4156034042e9259d5579edf4d35905fb06733
Michael Hartmeier <mlhartme@github.com>
2018-08-07 10:51:30 +0200

    work

--- src/main/java/net/oneandone/lavender/modules/Bitbucket.java
+++ src/main/java/net/oneandone/lavender/modules/Bitbucket.java
@@ -20,35 +20,41 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.oneandone.sushi.fs.World;
+import net.oneandone.sushi.fs.file.FileNode;
 import net.oneandone.sushi.fs.http.HttpNode;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/** As of 8-2018, we have bitbucket server 5.9.1.
- *  Rest API documentation: https://docs.atlassian.com/bitbucket-server/rest/5.12.0/bitbucket-rest.html?utm_source=%2Fstatic%2Frest%2Fbitbucket-server%2Flatest%2Fbitbucket-rest.html&utm_medium=301  */
+/**
+ * As of 8-2018, we have bitbucket server 5.9.1.
+ * <a href="https://docs.atlassian.com/bitbucket-server/rest/5.12.0/bitbucket-rest.html?utm_source=%2Fstatic%2Frest%2Fbitbucket-server%2Flatest%2Fbitbucket-rest.html&utm_medium=301">Rest API documentation</a>
+ */
+// CLI example: curl 'http://bitbucket.1and1.org:7990/rest/api/1.0/projects/CISOOPS/repos/puc/compare/changes?from=ea01f95dafd&to=cd309b8a877'  | python -m json.tool
 public class Bitbucket {
     public static void main(String[] args) throws IOException {
         World world;
         Bitbucket bitbucket;
-        JsonObject obj;
-        JsonArray values;
-        JsonObject path;
-        String full;
+        FileNode cache;
+
+        String cachedCommit;
+        String latestCommit;
+        List<String> files;
 
         world = World.create(false);
+        cache = world.file("cache");
         bitbucket = new Bitbucket((HttpNode) world.validNode("http://bitbucket.1and1.org:7990/rest/api/1.0"));
-        System.out.println("branches" + bitbucket.branches("CISOOPS", "puc"));
-        obj = bitbucket.changes("CISOOPS", "puc", "ea01f95dafd", "cd309b8a877");
-        values = obj.get("values").getAsJsonArray();
-        for (JsonElement element : values) {
-            path = element.getAsJsonObject().get("path").getAsJsonObject();
-            full = path.get("parent").getAsString();
-            if (!full.isEmpty()) {
-                full = full + "/";
-            };
-            full = full + path.get("name").getAsString();
-            System.out.println(full);
-
+        latestCommit = bitbucket.latestCommit("CISOOPS", "lavender-test-module", "master");
+        if (cache.exists()) {
+            System.out.println("no cache");
+            files = cache.readLines();
+            cachedCommit = files.remove(0);
+            files = bitbucket.changes("CISOOPS", "lavender-test-module", latestCommit, cachedCommit);
+            System.out.println("changed: " + files);
+        } else {
+            files = bitbucket.files("CISOOPS", "lavender-test-module", latestCommit);
+            System.out.println("files: " + files);
         }
     }
 
@@ -60,11 +66,63 @@ public class Bitbucket {
         this.parser = new JsonParser();
     }
 
-    public String branches(String project, String repository) throws IOException {
-        return root.join("projects", project, "repos", repository, "branches").readString();
+    public String latestCommit(String project, String repository, String branch) throws IOException {
+        JsonObject branches;
+        JsonArray values;
+        JsonObject obj;
+
+        branches = parser.parse(root.join("projects", project, "repos", repository, "branches").readString()).getAsJsonObject();
+        values = branches.get("values").getAsJsonArray();
+        for (JsonElement element : values) {
+            obj = element.getAsJsonObject();
+            if (branch.equals(obj.get("displayId").getAsString())) {
+                return obj.get("latestCommit").getAsString();
+            }
+        }
+        return null;
     }
 
-    public JsonObject changes(String project, String repository, String from, String to) throws IOException {
-        return parser.parse(root.getRoot().node(root.getPath() + "/projects/" + project + "/repos/" + repository + "/compare/changes", "from=" + from + "&to=" + to).readString()).getAsJsonObject();
+    /**
+     * @param from commit that contains the requested changes
+     * @param to   what to compare to, in my case the last revision I've seen
+     * @return list of files that have changed
+     */
+    public List<String> changes(String project, String repository, String from, String to) throws IOException {
+        JsonObject object;
+        JsonArray values;
+        JsonObject path;
+        String parent;
+        List<String> result;
+
+        result = new ArrayList<>();
+        object = parser.parse(root.getRoot().node(root.getPath() + "/projects/" + project + "/repos/" + repository + "/compare/changes", "from=" + from + "&to=" + to).readString()).getAsJsonObject();
+        values = object.get("values").getAsJsonArray();
+        for (JsonElement element : values) {
+            path = element.getAsJsonObject().get("path").getAsJsonObject();
+            parent = path.get("parent").getAsString();
+            if (!parent.isEmpty()) {
+                parent = parent + "/";
+            }
+            result.add(parent + path.get("name").getAsString());
+        }
+        return result;
+    }
+
+    /**
+     * @param at revision
+     */
+    // curl 'http://bitbucket.1and1.org:7990/rest/api/1.0/projects/CISOOPS/repos/puc/files'  | python -m json.tool
+    public List<String> files(String project, String repository, String at) throws IOException {
+        JsonObject object;
+        JsonArray values;
+        List<String> result;
+
+        object = parser.parse(root.getRoot().node(root.getPath() + "/projects/" + project + "/repos/" + repository + "/files", "at=" + at).readString()).getAsJsonObject();
+        values = object.get("values").getAsJsonArray();
+        result = new ArrayList<>();
+        for (JsonElement element : values) {
+            result.add(element.getAsString());
+        }
+        return result;
     }
 }
