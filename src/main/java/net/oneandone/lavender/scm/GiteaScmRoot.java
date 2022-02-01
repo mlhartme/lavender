69591152029afab8a7abf9f07bc6307e22c279f3
Michael Hartmeier <mlhartme.users.noreply@github.com>
2022-02-01 14:27:57 +0100

    giteascmroot

---
+++ src/main/java/net/oneandone/lavender/scm/GiteaScmRoot.java
@@ -0,0 +1,16 @@
+package net.oneandone.lavender.scm;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class GiteaScmRoot extends ScmRoot {
+    public GiteaScmRoot() {
+    }
+
+    public String getOrigin() {
+        return null;
+    }
+
+    public void writeTo(String path, OutputStream dest) throws IOException {
+    }
+}
